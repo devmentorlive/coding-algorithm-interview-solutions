a5cc1b708f1a7cef9e22bc0f652e7d2646884a2a
Mark Tellez <mark.tellez@icloud.com>
2020-11-14 12:47:03 -0600

    added bubble sort

---
+++ 4-bubble-sort.js
@@ -0,0 +1,14 @@
+function bubbleSort(nums) {
+  const sorted = [...nums];
+  for (let i = 0; i < sorted.length - 1; i++) {
+    for (let j = 0; j < sorted.length - i - 1; j++) {
+      if (sorted[j] > sorted[j + 1]) {
+        [sorted[j + 1], sorted[j]] = [sorted[j], sorted[j + 1]];
+      }
+    }
+  }
+
+  return sorted;
+}
+
+module.exports = bubbleSort;
